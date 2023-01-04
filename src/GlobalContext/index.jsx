f72e9f3e95aa729691195278ce899bed25a1a3e0
Tahir <dibirovtahir@gmail.com>
2023-01-04 16:33:22 +0400

    INIT

--- src/GlobalContext/index.jsx
+++ src/GlobalContext/index.jsx
@@ -8,7 +8,7 @@ export const GlobalProvider = ({children})=>{
 
     const price  = 125.00
     const [basket, setBasket] = useState(false)
-    const [image, setImage] = useState('/src/assets/img/image-product-1.jpg')
+    const [image, setImage] = useState('../src/assets/img/image-product-1.jpg')
     const [number, setNumber] = useState(1)
     const [totalPrice, setTotalPrice] = useState(price)
     
